38e35f6e795fe07424d23ec52116d9027c28ac31
aslisimsek <simseekasli@gmail.com>
2022-09-09 14:17:26 +0300

    second commit

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class MineSweeper<MineSweeperRules> {
     int row;
     int col;
     int countOfMineMine;
+    int gameArea;
 
-    public MineSweeper(int row, int col) {
+    public MineSweeperArea(int row, int col) {
         this.row = row;
         this.col = col;
         this.countOfMineMine = (row * col) / 4;
+        Random randomNumber = new Random();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please Enter a column");
@@ -20,25 +22,28 @@ public class MineSweeper<MineSweeperRules> {
     }
 
     public MineSweeperRules() {
-        int[][] mineMap;
-        int[][] baseMap;
-        int number = 1;
 
-        for (int i = 0; i < mineMap.length; i++)
-        {
-            int[] row = mineMap[i];
+        for (int i = 0; i < this.row; i++) {
+            for (int j = 0; j < this.col; j++) {
+                this.gameArea[i][j] = "-";
 
-            for (int j = 0; baseMap < row.length; j++)
-            {
-                row[j] = number;
-                number++;
 
             }
+        }
+
     }
+    public void minedMap() {
+        int locationMine = 0;
 
+        while (locationMine != countOfMineMine) {
+            int mineRow = (int) (Math.random() * this.row);
+            int mineCol = (int) (Math.random() * this.col);
 
+            if (this.minedMap();[mineRow][mineCol] != ("*")) {
+                countOfMineMine();[mineRow][mineCol] = "*";
+                locationMine++;
 
-
+}
 
 
 
